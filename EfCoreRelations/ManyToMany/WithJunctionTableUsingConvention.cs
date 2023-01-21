e503c633d04e80e791a41c199a6326ee94ffd87e
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-01-21 19:07:53 +0100

    -

---
+++ EfCoreRelations/ManyToMany/WithJunctionTableUsingConvention.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreRelations.ManyToMany.WithJunctionTableUsingConvention;
+
+public class Parent
+{
+    // Key
+    public int Id { get; set; } // Blir PK genom konvention, pga namnet.
+
+    // Nav
+    public ICollection<Parent_Child> Parent_Child { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+public class Child
+{
+    // Key
+    public int Id { get; set; } // Blir PK genom konvention, pga namnet.
+
+    // Nav
+    public ICollection<Parent_Child> Parent_Child { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+public class Parent_Child // Kopplingstabell som får en-till-många relation till både parent och child.
+{
+    // Key
+    public int ParentId { get; set; } // Blir FK genom konvention, pga namnet.
+    public int ChildId { get; set; } // Blir FK genom konvention, pga namnet.
+
+    // Nav
+    public Parent Parent { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+    public Child Child { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+
+
+public class ManyToManyWithJunctionTableUsingConventionContext : BaseContext
+{
+    public DbSet<Parent> Parents { get; set; }
+    public DbSet<Child> Children { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Kompositnyckeln i kopplingstabellen måste anges med fluent.
+        modelBuilder.Entity<Parent_Child>()
+            .HasKey(e => new {e.ParentId, e.ChildId });
+    }
+}
---
+++ EfCoreRelations/ManyToMany/WithJunctionTableUsingFluent1.cs
@@ -0,0 +1,62 @@
+﻿// Vanliga versionen med navigation properties.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreRelations.ManyToMany.WithJunctionTableUsingFluent1;
+
+public class Parent
+{
+    // Key
+    public int MyCustomPK { get; set; }
+
+    // Nav
+    public ICollection<Parent_Child> Parent_Child { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+public class Child
+{
+    // Key
+    public int MyCustomPK { get; set; }
+
+    // Nav
+    public ICollection<Parent_Child> Parent_Child { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+public class Parent_Child // Kopplingstabell som får en-till-många relation till både parent och child.
+{
+    // Key
+    public int MyCustomParentFK { get; set; }
+    public int MyCustomChildFK { get; set; }
+
+    // Nav
+    public Parent Parent { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+    public Child Child { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+
+
+public class ManyToManyWithJunctionTableUsingFluent1Context : BaseContext
+{
+    public DbSet<Parent> Parents { get; set; }
+    public DbSet<Child> Children { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Child>()
+            .HasKey(e => e.MyCustomPK);
+
+        modelBuilder.Entity<Parent>()
+            .HasKey(e => e.MyCustomPK);
+
+        modelBuilder.Entity<Parent_Child>()
+            .HasKey(e => new { e.MyCustomParentFK, e.MyCustomChildFK });
+        modelBuilder.Entity<Parent_Child>()
+            .HasOne(e => e.Child)
+            .WithMany(e => e.Parent_Child)
+            .HasForeignKey(e => e.MyCustomChildFK);
+        modelBuilder.Entity<Parent_Child>()
+            .HasOne(e => e.Parent)
+            .WithMany(e => e.Parent_Child)
+            .HasForeignKey(e => e.MyCustomParentFK);
+    }
+}
