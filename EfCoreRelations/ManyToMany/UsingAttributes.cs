bd1b8c6537341e8f7af80d59d97663c3a7aeb244
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-01-21 18:39:59 +0100

    -

---
+++ EfCoreRelations/ManyToMany/UsingAttributes.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreRelations.ManyToMany.UsingAttributes;
+
+public class Parent
+{
+    // Key
+    [Key]
+    public int MyCustomPK { get; set; }
+
+    // Nav
+    public ICollection<Child> Children { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+public class Child
+{
+    // Key
+    [Key]
+    public int MyCustomPK { get; set; }
+
+    // Nav
+    public ICollection<Parent> Parents { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+
+
+public class ManyToManyUsingAttributesContext : BaseContext
+{
+    public DbSet<Parent> Parents { get; set; }
+    public DbSet<Child> Children { get; set; }
+}
---
+++ EfCoreRelations/ManyToMany/UsingFluent1.cs
@@ -0,0 +1,43 @@
+﻿// Vanliga versionen med navigation properties.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreRelations.ManyToMany.UsingFluent1;
+
+public class Parent
+{
+    // Key
+    public int MyCustomPK { get; set; }
+
+    // Nav
+    public ICollection<Child> Children { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+public class Child
+{
+    // Key
+    public int MyCustomPK { get; set; }
+
+    // Nav
+    public ICollection<Parent> Parents { get; set; } // Navigation property som EF tolkar för att skapa relationen.
+}
+
+
+
+public class ManyToManyUsingFluent1Context : BaseContext
+{
+    public DbSet<Parent> Parents { get; set; }
+    public DbSet<Child> Children { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Child>()
+            .HasKey(e => e.MyCustomPK);
+        modelBuilder.Entity<Child>()
+            .HasMany(e => e.Parents)
+            .WithMany(e => e.Children);
+
+        modelBuilder.Entity<Parent>()
+            .HasKey(e => e.MyCustomPK);
+    }
+}
