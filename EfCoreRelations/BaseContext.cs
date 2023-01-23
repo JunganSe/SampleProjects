ab336412209ef8c312f95213044045dfd3de29e5
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-01-23 14:08:55 +0100

    -

--- EfCoreRelations/BaseContext.cs
+++ EfCoreRelations/BaseContext.cs
@@ -3,9 +3,19 @@
 namespace EfCoreRelations;
 public class BaseContext : DbContext
 {
+    public BaseContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
-            optionsBuilder.UseInMemoryDatabase("Bahuga");
+        {
+            //optionsBuilder.UseInMemoryDatabase("EfCoreRelations");
+            Directory.CreateDirectory("Database");
+            optionsBuilder.UseSqlite("Data Source=Database/EfCoreRelations.sqlite");
+        }
     }
 }
