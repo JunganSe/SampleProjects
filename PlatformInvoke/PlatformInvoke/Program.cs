e4a3d4e0dd0a1622f85b4cb503376a97a52d8e39
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-05-20 21:41:25 +0200

    Init PlatformInvoke

---
+++ PlatformInvoke/PlatformInvoke/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PlatformInvoke
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
