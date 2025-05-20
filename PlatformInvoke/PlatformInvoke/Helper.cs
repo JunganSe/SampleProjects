3f1e306984ad318e2aec08a235dcae3ad42d27b1
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-05-20 22:00:12 +0200

    P/invoke

---
+++ PlatformInvoke/PlatformInvoke/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PlatformInvoke;
+
+internal static class Helper
+{
+    public static void DisplayLastError()
+    {
+        // Get the last p/invoke error and display it.
+        int error = Marshal.GetLastPInvokeError();
+        Console.WriteLine($"Last p/invoke error: {error}");
+    }
+}
---
+++ PlatformInvoke/PlatformInvoke/UsingDllImport.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PlatformInvoke;
+
+internal class UsingDllImport
+{
+    [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+    public void ShowMessageBox()
+    {
+        int hResult = MessageBox(IntPtr.Zero, "Using DllImport", "Example caption", 0);
+        Console.WriteLine($"hResult: {hResult}");
+
+        Helper.DisplayLastError();
+    }
+}
