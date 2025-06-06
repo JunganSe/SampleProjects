469103343f3cf8255d4f530293d51376772b7f64
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-06-06 17:30:50 +0200

    -

--- PlatformInvoke/PlatformInvoke/Program.cs
+++ PlatformInvoke/PlatformInvoke/Program.cs
@@ -1,15 +1,14 @@
-﻿namespace PlatformInvoke
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var dllImport = new UsingDllImport();
-            dllImport.ShowMessageBox();
+﻿namespace PlatformInvoke;
 
-            var libraryImport = new UsingLibraryImport();
-            libraryImport.ShowMessageBox();
-            libraryImport.ShowMessageBox2();
-        }
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var dllImport = new UsingDllImport();
+        dllImport.ShowMessageBox();
+
+        var libraryImport = new UsingLibraryImport();
+        libraryImport.ShowMessageBox();
+        libraryImport.ShowMessageBox2();
     }
 }
