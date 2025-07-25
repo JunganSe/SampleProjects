02206d1406db8fae53d01cd68b4096be61939610
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-07-25 18:11:05 +0200

    Call the parser

--- CommandLineArguments/CommandLineArguments/Program.cs
+++ CommandLineArguments/CommandLineArguments/Program.cs
@@ -4,6 +4,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var arguments = BooleanArgumentsParser.Parse<SampleArguments>(args);
     }
 }
