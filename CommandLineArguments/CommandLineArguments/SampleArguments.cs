9ca21925427d3541923c3ada2c6727773e80fc9c
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-07-25 18:11:00 +0200

    Add SampleArguments

---
+++ CommandLineArguments/CommandLineArguments/SampleArguments.cs
@@ -0,0 +1,8 @@
+﻿namespace CommandLineArguments;
+
+internal class SampleArguments
+{
+    public bool SampleArg1 { get; set; }
+    public bool SampleArg2 { get; set; }
+    public bool SampleArg3 { get; set; }
+}
